55abe33dcf15eccec7ab105292ef6029f6b737c6
hafizh1234 <haf12h345@gmail.com>
2021-06-02 21:08:30 +0700

    first commit

---
+++ app/src/main/java/com/projek/sampahku1/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.projek.sampahku1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.projek.sampahku1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnLogin.setOnClickListener{
+            var intent= Intent(this@MainActivity,LoginActivity::class.java)
+            startActivity(intent)
+
+        }
+        binding.btnRegister.setOnClickListener{
+            var intent=Intent(this@MainActivity,RegisterActivity::class.java)
+        }
+    }
+}
